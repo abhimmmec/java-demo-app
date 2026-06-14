03c53f3c66f2147d577bc9556726f817bc551883
abhimmmec <vermaabhishekmmmec@gmail.com>
2026-06-14 21:58:48 +0530

    Refactor home method logging in JenkinController

--- src/main/java/com/example/abhishek/controller/JenkinController.java
+++ src/main/java/com/example/abhishek/controller/JenkinController.java
@@ -1,22 +1,22 @@
 package com.example.abhishek.controller;
 
-import java.awt.Desktop;
-import java.io.File;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class JenkinController {
 
-	 @GetMapping("/")
-	    public String home() {
-			 logger.info("Home page request received- rahul ki gaand fad di ");
-        logger.info("Application triggered successfully from Jenkins build by abhishek");
-	        return "index";
-	    }
-	  
+    private static final Logger logger = LoggerFactory.getLogger(JenkinController.class);
 
+    @GetMapping("/")
+    public String home() {
+
+        logger.info("Home page request received");
+        logger.info("Application triggered successfully from Jenkins build");
+
+        return "index";
+    }
 }
